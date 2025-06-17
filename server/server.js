ac5af157e51eb8d3fc8a17b7d6c7a923bbef8040
Miha Lamot <miha.lamot@gmail.com>
2025-06-17 14:23:31 +0200

    finished server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
-require('dotenv').config();
+require('dotenv').config();     //looks for .env file
 
-const express = require('express');
+const express = require('express');   
 
-const app = express();
+const app = express();     //creates express instance for routes and middleware
 
-app.get('/', (req, res) => {
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();   //next to move to the next middleware or route handler
+});
+
+//next middleware
+app.get('/', (req, res) => {      //  '/' is root path
     res.json({
         message: 'json message',
     });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(process.env.PORT, () => {     //access to env: process.env.variable
+  console.log('Server is running on http://localhost:' + process.env.PORT);
 });
 
